aa6a3fa9f117e0cff0c0397bdb8c3bf4fa9a2d47
Natalia <nata.vlasiuk17@gmail.com>
2022-08-18 22:06:27 -0500

    one more commit

---
+++ src/day1/HelloWorld.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+//some comments added
+
+
+
+
+
+
+
+
+
+
+    }
+}
